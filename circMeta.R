028c6d46587e332c0194c9b64aded80e5e4e4cb4
Jacqueline Griffin <JGriffin@RUBIN4.HITCHCOCK.ORG>
2020-02-20 17:38:45 -0500

    f

--- circMeta.R
+++ circMeta.R
@@ -9,4 +9,12 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("BSgenome.Hsapiens.UCSC.hg38")
-circJuncDE(files,designs, circ.method=c('findcirc','CIRCexplorer','CIRI'), DE.method=c('pois.ztest','DESeq2','edgeR','pois.glm','nb.glm'), gene=NULL,gexon=NULL, cutoff=2,sf=TRUE)
+
+
+ciri_26 <- read.delim("26_ciri_ud.ciri", header = TRUE, stringsAsFactors = TRUE, row.names = 1)
+
+circJuncDE(ciri_26,designs, circ.method=c('findcirc','CIRCexplorer','CIRI'),
+           DE.method=c('pois.ztest','DESeq2','edgeR','pois.glm','nb.glm'), 
+           gene=NULL,gexon=NULL, cutoff=2,sf=TRUE)
+
+head(ciri_26)
