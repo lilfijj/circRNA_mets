0ab55b2cd9373bb15d24c9f43aa134c5bae55783
Jacqueline Griffin <JGriffin@RUBIN4.HITCHCOCK.ORG>
2020-02-27 10:36:38 -0500

    2 27

--- ularcirc_final.R
+++ ularcirc_final.R
@@ -1,42 +1,38 @@
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
+
 BiocManager::install("AnnotationHub")
 library(AnnotationHub)
-
+install.packages("digest")
 # https://github.com/VCCRI/Ularcirc - circRNA after BWA-MEM, CIRI2
 
-install.packages("devtools")
+
+
+if(!require("devtools")) {install.packages("devtools")}
 library(devtools)
 devtools::install_github("VCCRI/Ularcirc", 
                            build = TRUE, build_vignettes = TRUE, build_opts = c("--no-resave-data", "--no-manual"))
 
 library("Ularcirc")
-all_dbs <- Compatible_Annotation_DBs() # This will return all compatible databases
-
-#mmu_dbs <- Compatible_Annotation_DBs(search_term = 'mm10') # returns mm10 compatible databases
-install.packages("devtools")
-library(devtools)
-install.packages('nlme')
-library(nlme)
-install.packages('MASS')
-library(MASS)
-
-install.packages('lattice')
-library(lattice)
-install.packages('foreign')
-library(foreign)
+all_dbs <- Compatible_Annotation_DBs(localHub=FALSE) # This will return all compatible databases
 
 
-install.packages('boot')
-library(boot)
+setAnnotationHubOption("PROXY","https://username:password@proxy:port")
+
+ah = AnnotationHub()
 
 
-install.packages("vctrs")
+mmu_dbs <- Compatible_Annotation_DBs(search_term = 'hg38') # returns mm10 compatible databases
+
+
+
+if(!require("vctrs")) {install.packages("vctrs")}
+library('vctrs')
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-BiocManager::install()
 BiocManager::install("BiocStyle")
+library("BiocStyle")
+
+
 devtools::install_github("VCCRI/Ularcirc", build = TRUE, build_vignettes = TRUE, build_opts = c("--no-resave-data", "--no-manual"))
 library("Ularcirc")
 all_dbs <- Compatible_Annotation_DBs() # This will return all compatible databases
